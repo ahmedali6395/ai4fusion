11fe5fa90107f69393f35a04a7d9942fd29ecb54
Ali Ahmed <aliahmed@Alis-MacBook-Pro.local>
2026-06-11 01:31:03 -0400

    filtering and basic NN to be tested thursday morning

--- utils/createLoader.py
+++ utils/createLoader.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import json
 import matplotlib.pyplot as plt
+from scipy import stats
 
 from sklearn.preprocessing import MinMaxScaler
 
@@ -24,14 +25,36 @@ def getDatasetY(dataset, config):
 # }
 
 def createLoaders(config, dataset, regression=False):
+    """Create training, testing, and validation dataloaders
+
+    Args:
+        config (.json): _description_
+        dataset (pd Dataset): pandas dataset
+        regression (bool, optional): Regression mode. Defaults to False.
+
+    Returns:
+        DataLoader: training_loader
+        DataLoader: testing_loader
+        DataLoader: validation_loader
+    """
 # {
     dataset = dataset.replace(np.inf, np.nan);
     dataset = dataset.replace(-np.inf, np.nan);
 
     dataset = cropData(dataset, config).dropna();
-
+    
+    
+    if config["gaussian_filter"]["filter_by_gaussian"]: 
+        dataset = dataset[(np.abs(stats.zscore(dataset.select_dtypes(include=[np.number]))) < config["gaussian_filter"]["std_dev"]).all(axis=1)]
+    
     datasetX = getDatasetX(dataset, config);
     datasetY = getDatasetY(dataset, config);
+    
+
+    if config["plot"]:
+        datasetX.hist(figsize=(15,15),bins=50,density=True)
+
+
 
     # if useScale
     if config["dataloader"]["scale"]:
@@ -41,6 +64,8 @@ def createLoaders(config, dataset, regression=False):
         featureScaler.fit(datasetX);
         datasetX = featureScaler.transform(datasetX.astype('float32'));
         datasetX = torch.from_numpy(datasetX);
+
+        
 
         if regression:
         # {
@@ -69,6 +94,20 @@ def createLoaders(config, dataset, regression=False):
     # {
         # TODO
         torch.histogram(datasetX, bins=10);
+        
+        to_plot = np.arange(0, (datasetX).shape[1]-1)
+    
+        fig, axes = plt.subplots(6, 8, figsize=(16, 12))
+        axes = axes.ravel()  # flatten into 1D array
+        
+        for i in to_plot: 
+            data_to_plot = datasetX[:, i].detach().cpu().numpy()
+            axes[i].hist(data_to_plot, bins=50)
+            axes[i].set_title(f"Sample {i}")
+    
+                
+
+        
     # }
 
     training_loader = torch.utils.data.DataLoader(
